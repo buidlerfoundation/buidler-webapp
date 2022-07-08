000a79c51fa16a1e1b1133f373234d3f6204b7b6
Ryan <tai.htm@gmail.com>
2022-07-08 13:24:03 +0700

    Update docker script

--- docker/prod.sh
+++ docker/prod.sh
@@ -2,6 +2,6 @@
 
 _dockerDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-exec docker-compose -p buidler-webapp-prod \
+exec docker compose -p buidler-webapp-prod \
   -f "$_dockerDir/compose.prod.yml" \
   "$@"
