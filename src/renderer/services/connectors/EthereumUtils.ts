12887c1d469fa326fdceae95d189953218cbc580
HungNguyen <baohung2412@gmail.com>
2022-10-28 10:46:25 +0700

    Update modal send

---
+++ src/renderer/services/connectors/EthereumUtils.ts
@@ -0,0 +1,7 @@
+import { SendData } from "renderer/models";
+
+class EthereumUtils {
+  sendERC721Transaction = async (sendData: SendData, from: string) => {};
+}
+
+export default new EthereumUtils();
