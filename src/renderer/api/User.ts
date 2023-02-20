2a8d7f52f3810a8a8254d340b342a608d1872f1b
HungNguyen <baohung2412@gmail.com>
2023-02-20 09:52:55 +0700

    Increate limit member data

--- src/renderer/api/User.ts
+++ src/renderer/api/User.ts
@@ -158,7 +158,7 @@ export const getMembersByRole = (
   } = {}
 ) => {
   const { userName, page } = params;
-  let url = `team/${teamId}/role?page=${page || 1}&limit=50`;
+  let url = `team/${teamId}/role?page=${page || 1}&limit=80`;
   roles.forEach((el) => {
     url += `&roles[]=${el}`;
   });
