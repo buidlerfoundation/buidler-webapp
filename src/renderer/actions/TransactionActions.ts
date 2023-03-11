74560dc26b6354833e265229c7e4df58e0464692
HungNguyen <baohung2412@gmail.com>
2023-03-11 20:22:52 +0700

    Temporary disable transaction list api

--- src/renderer/actions/TransactionActions.ts
+++ src/renderer/actions/TransactionActions.ts
@@ -5,18 +5,18 @@ import actionTypes from "./ActionTypes";
 export const getTransactions: ActionCreator<any> =
   (page: number, isFresh = false) =>
   async (dispatch: Dispatch) => {
-    if (page > 1) {
-      dispatch({ type: actionTypes.TRANSACTION_MORE });
-    } else if (isFresh) {
-      dispatch({ type: actionTypes.TRANSACTION_FRESH });
-    } else {
-      dispatch({ type: actionTypes.TRANSACTION_REQUEST });
-    }
-    const res = await api.fetchTransaction({ page });
-    if (res.statusCode === 200) {
-      dispatch({
-        type: actionTypes.TRANSACTION_SUCCESS,
-        payload: { data: res.data, page, canMore: res.data?.length === 20 },
-      });
-    }
+    // if (page > 1) {
+    //   dispatch({ type: actionTypes.TRANSACTION_MORE });
+    // } else if (isFresh) {
+    //   dispatch({ type: actionTypes.TRANSACTION_FRESH });
+    // } else {
+    //   dispatch({ type: actionTypes.TRANSACTION_REQUEST });
+    // }
+    // const res = await api.fetchTransaction({ page });
+    // if (res.statusCode === 200) {
+    //   dispatch({
+    //     type: actionTypes.TRANSACTION_SUCCESS,
+    //     payload: { data: res.data, page, canMore: res.data?.length === 20 },
+    //   });
+    // }
   };
