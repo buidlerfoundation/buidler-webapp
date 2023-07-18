6ddcb232f9e762bfbc90a68502b40b1b2fa73c79
HungNguyen <baohung2412@gmail.com>
2023-07-18 12:29:59 +0700

    Fix topic data

--- src/hooks/useStoryDataById.ts
+++ src/hooks/useStoryDataById.ts
@@ -4,7 +4,7 @@ import useChannel from "./useChannel";
 
 function useStoryDataById(id?: string | null) {
   const channel = useChannel();
-  const topicData = useTopicData(channel?.dapp_integration_url);
+  const topicData = useTopicData(channel?.display_channel_url);
   return useMemo(
     () => topicData?.stories?.find((el) => el.id === id),
     [id, topicData?.stories]
