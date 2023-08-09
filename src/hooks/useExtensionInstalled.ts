bb267dd3e7dcea5821b6878dda2862aae5d4eb8d
HungNguyen <baohung2412@gmail.com>
2023-08-09 22:28:01 +0700

    Check extension installed

---
+++ src/hooks/useExtensionInstalled.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const useExtensionInstalled = () => {
+  const [installed, setInstalled] = useState(true);
+  useEffect(() => {
+    const isInstalled =
+      document.documentElement.getAttribute("buidler-extension");
+    setInstalled(!!isInstalled);
+  }, []);
+  return installed;
+};
+
+export default useExtensionInstalled;
