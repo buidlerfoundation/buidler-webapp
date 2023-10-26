491f698b27af69f259114fd1295d6c3ab5a67fde
HungNguyen <baohung2412@gmail.com>
2023-10-26 15:51:53 +0700

    Update modal compose

--- src/helpers/CastHelper.ts
+++ src/helpers/CastHelper.ts
@@ -1,6 +1,6 @@
 import { ICast } from "models/FC";
 
-const regexUrl =
+export const regexUrl =
   /((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,256}\b([-a-zA-Z0-9@:%_+.~#(?&//=]*))/gim;
 
 export const insertHttpIfNeed = (str?: string) => {
