86d14c747cf586e9454d7ea6c1250513f903f408
HungNguyen <baohung2412@gmail.com>
2023-11-28 11:23:16 +0700

    Update metadata title

--- src/app/insights/[username]/page.tsx
+++ src/app/insights/[username]/page.tsx
@@ -1,3 +1,22 @@
+import CallerServer from "api/CallerServer";
+import { IFCUser } from "models/FC";
+import { Metadata, ResolvingMetadata } from "next";
 import UserAnalytic from "screens/UserAnalytic";
+
+export async function generateMetadata(
+  { username }: { username: string },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const userByUserNameRes = await CallerServer.get<IFCUser>(
+    `users/user-by-username?${new URLSearchParams({ username })}`
+  );
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `${userByUserNameRes?.data?.display_name} (@${userByUserNameRes?.data?.username}) | Buidler`,
+  };
+}
 
 export default UserAnalytic;
