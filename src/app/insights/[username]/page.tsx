85ab1ef701004b7de157fcf371b705fa0cd00d26
HungNguyen <baohung2412@gmail.com>
2023-11-28 11:28:31 +0700

    Update metadata

--- src/app/insights/[username]/page.tsx
+++ src/app/insights/[username]/page.tsx
@@ -4,18 +4,30 @@ import { Metadata, ResolvingMetadata } from "next";
 import UserAnalytic from "screens/UserAnalytic";
 
 export async function generateMetadata(
-  { username }: { username: string },
+  {
+    params,
+  }: {
+    params: { username: string };
+  },
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   const userByUserNameRes = await CallerServer.get<IFCUser>(
-    `users/user-by-username?${new URLSearchParams({ username })}`
+    `users/user-by-username?${new URLSearchParams(params)}`
   );
 
   // optionally access and extend (rather than replace) parent metadata
   // const previousImages = (await parent).openGraph?.images || [];
 
+  const title = `${userByUserNameRes?.data?.display_name} (@${userByUserNameRes?.data?.username}) | Buidler`;
+  const description = userByUserNameRes?.data?.profile?.bio?.text;
+
   return {
-    title: `${userByUserNameRes?.data?.display_name} (@${userByUserNameRes?.data?.username}) | Buidler`,
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
   };
 }
 
