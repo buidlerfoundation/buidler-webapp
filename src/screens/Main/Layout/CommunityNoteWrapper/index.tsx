d925428c3f0ed3a8f39dbabfc4fb3a0ad60c487b
HungNguyen <baohung2412@gmail.com>
2023-12-22 10:11:21 +0700

    Update community note wrapper

--- src/screens/Main/Layout/CommunityNoteWrapper/index.tsx
+++ src/screens/Main/Layout/CommunityNoteWrapper/index.tsx
@@ -2,39 +2,13 @@
 
 import React, { memo } from "react";
 import styles from "./index.module.scss";
-import { usePathname } from "next/navigation";
-import useCommunityNoteFilters from "hooks/useCommunityNoteFilters";
-import Link from "next/link";
 
 interface ICommunityNoteWrapper {
   children: React.ReactNode;
 }
 
 const CommunityNoteWrapper = ({ children }: ICommunityNoteWrapper) => {
-  const filters = useCommunityNoteFilters();
-  const pathname = usePathname();
-  return (
-    <div className={styles.container}>
-      <nav className={styles.nav}>
-        <div className={styles.head}>Community Notes</div>
-        <div className={styles.tabs}>
-          {filters?.map((el) => (
-            <Link
-              href={`/plugin-fc${el.path}`}
-              key={el.path}
-              className={`${styles["tab-item"]} ${
-                pathname === `/plugin-fc${el.path}` ? styles.active : ""
-              }`}
-              replace
-            >
-              {el.label}
-            </Link>
-          ))}
-        </div>
-      </nav>
-      {children}
-    </div>
-  );
+  return <div className={styles.container}>{children}</div>;
 };
 
 export default memo(CommunityNoteWrapper);
